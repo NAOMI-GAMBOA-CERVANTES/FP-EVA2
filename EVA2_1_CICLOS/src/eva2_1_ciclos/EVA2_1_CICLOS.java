f5787d1672ab4525d35947075373544502082dff
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-10-02 16:15:06 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //REPETICIÓN DE INSTRUCCIONES
+        //CICLOS O BUCLES
+        //CICLO FOR
+        for(int i = 1; i <= 10; i++){
+            // ++ --> operador para sumar 1 unidad
+            // -- --> resta una unidad
+            // += suma N valor
+            // -= resta N
+            // *= Multiplica por N
+            // /= División entre N
+        }
+    }
+    
+}
