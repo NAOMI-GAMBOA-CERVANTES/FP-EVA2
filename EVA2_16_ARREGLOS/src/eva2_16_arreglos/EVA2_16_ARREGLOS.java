0a7d28b5d2d744701bd24528fa6272bebed596ca
Naomi <L24550254@chihuahua2.tecnm.mx>
2024-10-21 16:51:02 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_16_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_16_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] diasSemana = {"Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
+                
+        //IMPRIMIR
+        //El for esta hecho para trabajar con arreglos (Se puede usar otro método)
+        
+        for (int i = 0; i < diasSemana.length; i++){
+            System.out.println(diasSemana[i]);
+    }
+    
+}
+}
