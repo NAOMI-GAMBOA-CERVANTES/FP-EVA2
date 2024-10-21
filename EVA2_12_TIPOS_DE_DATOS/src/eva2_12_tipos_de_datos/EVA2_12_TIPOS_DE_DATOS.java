62b0f11e2f6f2505daa31c13197ca03b2ef3c1fa
Naomi <L24550254@chihuahua2.tecnm.mx>
2024-10-21 16:43:46 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DE_DATOS/src/eva2_12_tipos_de_datos/EVA2_12_TIPOS_DE_DATOS.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_tipos_de_datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_TIPOS_DE_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //byte = 8 bits
+        byte valor = -128;
+        byte valor2 = 127;
+        System.out.println("Valor = " + valor);
+        System.out.println("Valor2 = " + valor2);
+        //short
+        short val = -32768;
+        short val2 = 32767;
+        //int
+        int x = -2147483618;
+        int y = 2147483617;
+        
+        
+    }
+    
+}
